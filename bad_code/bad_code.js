5ee9290027682e830005202e541b09b1889a1413
dhruv1410 <dhruv.agrawal2022@gmail.com>
2025-12-13 06:49:24 +0530

    chore: triggering github actions for testing

--- bad_code/bad_code.js
+++ bad_code/bad_code.js
@@ -3,11 +3,11 @@
 function  messyFunction( a,b ){
 var unused_variable = "I am never used";
     const x=10
-  const y = 20;;;;;;;
+  const y = 20;
 
-      if(x==10){
+                if(x==10){
 return a+b
       }
 }
-;
+;;;
 var result = messyFunction( 5,10 );;;
